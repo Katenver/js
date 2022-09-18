1018994c38fc6bef7a6b26d77071799e6f0436cf
Kateryna Nasurlaieva <8711724@ukr.net>
2022-09-18 12:55:21 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-const guestName = "Манго";
-const roomNumber = 207;
-const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
-console.log(greeting);
+// const guestName = "Манго";
+// const roomNumber = 207;
+// const greeting = `Welcome ${guestName}, your room number is ${roomNumber}!`;
+// console.log(greeting);
+
+function getEvenNumbers(start, end) {
+    
+ let arr = [];
+ 
+ for (let i = start; i < end; i += 1) {
+   const number = numbers[i];
+
+   if (number % 2 === 0) {
+ arr.push(number)}
+ 
+ return arr
+   
+   }}
+  
